8305af26a65fee8f07cd02395cb8ba186d689eb1
kirik18119 <trosin.kirik@gmail.com>
2024-07-26 13:02:12 +0300

    Database creation v1.1

--- dump.sql
+++ dump.sql
@@ -2,8 +2,8 @@ USE task_manager;
 
 CREATE TABLE users(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(20) NOT NULL
 );
@@ -13,8 +13,8 @@ CREATE TABLE tasks(
     title VARCHAR(255),
     description TEXT,
     assigned_user INT,
-    date_time_of_creation DATETIME,
-    date_time_of_last_update DATETIME,
-    status VARCHAR(10),
+    created_at DATETIME,
+    updated_at DATETIME,
+    status ENUM('NEW','IN PROGRESS','REVIEW','DONE'),
     FOREIGN KEY (assigned_user) REFERENCES users(id)
 );
