a415c3b12aaaa75d6cf0fa7d38f269ede149007c
vanya_php <LodnyaI@eurotorg.by>
2025-12-03 17:42:21 +0300

    Fixes for database definition inside dump file

--- mysql-dump.sql
+++ mysql-dump.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users
 (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(10) NOT NULL,
-    last_name VARCHAR(10) NOT NULL,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
     is_admin BOOLEAN,
-    category VARCHAR(20) NOT NULL
+    category TINYINT(10) NOT NULL
 );
 
 CREATE TABLE tasks
@@ -12,9 +12,10 @@ CREATE TABLE tasks
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR (50) NOT NULL,
     description TEXT,
-    status VARCHAR(20) DEFAULT 'to-do',
+    status TINYINT(10) DEFAULT 1,
     estimated_hours INT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted_at DATETIME DEFAULT NULL
 );
 
@@ -22,45 +23,44 @@ CREATE TABLE task_progress
 (
     id INT AUTO_INCREMENT,
     task_id INT,
-    FOREIGN KEY (task_id) REFERENCES tasks (id),
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES users (id),
     name VARCHAR(40) NOT NULL,
-    spent_hours INT
-
+    spent_hours INT,
+    FOREIGN KEY (task_id) REFERENCES tasks (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE task_files
 (
     id INT AUTO_INCREMENT,
     task_id INT,
-    FOREIGN KEY (task_id) REFERENCES tasks (id),
-    file_path VARCHAR(100) NOT NULL
+    file_path TEXT NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES tasks (id)
 );
 
 CREATE TABLE task_status_history
 (
     id INT AUTO_INCREMENT,
     task_id INT,
-    FOREIGN KEY (task_id) REFERENCES tasks (id),
-    old_status VARCHAR(20) NOT NULL ,
-    new_status VARCHAR(20) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    old_status TINYINT(10) NOT NULL ,
+    new_status TINYINT(10) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (task_id) REFERENCES tasks (id)
 );
 CREATE TABLE task_assigment_history
 (
     id INT AUTO_INCREMENT,
     task_id INT,
-    FOREIGN KEY (task_id) REFERENCES tasks (id),
     old_user_id INT,
-    FOREIGN KEY (old_user_id) REFERENCES users (id),
     new_user_id INT,
+    FOREIGN KEY (task_id) REFERENCES tasks (id),
+    FOREIGN KEY (old_user_id) REFERENCES users (id),
     FOREIGN KEY (new_user_id) REFERENCES users (id)
 );
 CREATE TABLE comments
 (
     id INT AUTO_INCREMENT,
-    task_id INT ,
-    FOREIGN KEY (task_id) REFERENCES tasks (id),
-    text TEXT NOT NULL
-)
+    task_id INT,
+    text TEXT NOT NULL,
+    FOREIGN KEY (task_id) REFERENCES tasks (id)
+);
