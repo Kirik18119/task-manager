be4d582d8c42794c7fb5dc8914e5965016392886
vanya_php <LodnyaI@eurotorg.by>
2025-12-12 10:34:40 +0300

    Added phpunit lib + query builder new feature for database fetching with relations + mapping raw relation data to Model classes

--- src/Core/ORM/QueryBuilder.php
+++ src/Core/ORM/QueryBuilder.php
@@ -4,6 +4,10 @@ namespace Core\ORM;
 
 use Core\Collection\ArrayCollection;
 use Core\Database;
+use Core\ORM\Relation\BelongTo;
+use Core\ORM\Relation\HasMany;
+use Core\ORM\Relation\HasOne;
+use Exception;
 
 class QueryBuilder
 {
@@ -17,16 +21,54 @@ class QueryBuilder
         private readonly string $primaryKey
     )
     {
-        $this->rawSql = sprintf("SELECT %s FROM %s", implode(',', $this->selectedColumns), $this->tableName);
-    }
-
-    public function with(array $relations)
-    {
-
+        $this->rawSql = sprintf("SELECT %s FROM %s ", implode(',', $this->selectedColumns), $this->tableName);
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
+     */
+    public function with(array $relations)
+    {
+        $modelTempObject = new $this->modelClassName;
+        foreach ($relations as $relation)
+        {
+            if (!method_exists($this->modelClassName, $relation))
+            {
+                throw new Exception(sprintf('Relation %s is not defined for %s', $relation, $this->modelClassName));
+            }
+
+            $relationInstance = $modelTempObject->{$relation}();
+            if ($relationInstance instanceof BelongTo)
+            {
+                $this->rawSql .= sprintf("LEFT JOIN %s ON %s.%s = %s.%s ",
+                    $relationInstance->parentClassName::$table,
+                    $this->modelClassName::$table,
+                    $relationInstance->foreignKeyName,
+                    $relationInstance->parentClassName::$table,
+                    $relationInstance->parentClassName::$primaryKey
+                );
+            }
+            else if ($relationInstance instanceof HasOne || $relationInstance instanceof HasMany)
+            {
+                $this->rawSql .= sprintf("LEFT JOIN %s ON %s.%s = %s.%s ",
+                    $relationInstance->childClassName::$table,
+                    $this->modelClassName::$table,
+                    $this->modelClassName::$primaryKey,
+                    $relationInstance->childClassName::$table,
+                    $relationInstance->localKeyName
+                );
+            }
+            else
+            {
+                throw new Exception("Undefined type of relation for %s relation", $relation);
+            }
+            unset($relationInstance);
+        }
+        unset($modelTempObject);
+    }
+
+    /**
+     * @throws Exception
      */
     public function get(): ArrayCollection
     {
--- src/Core/ORM/Trait/CanMap.php
+++ src/Core/ORM/Trait/CanMap.php
@@ -4,15 +4,33 @@ namespace Core\ORM\Trait;
 
 use Core\Collection\ArrayCollection;
 use Core\ORM\Model;
+use Core\ORM\Relation\BelongTo;
+use Core\ORM\Relation\HasMany;
+use Core\ORM\Relation\HasOne;
 use Exception;
 
 trait CanMap
 {
+    /**
+     * @throws Exception
+     */
     public static function mapRawToModel(array $rawData): Model
     {
         $model = new static();
         foreach ($rawData as $field => $value) {
-            if (array_key_exists($field, static::$casts)) {
+            if (is_array($value)) {
+                $relationInstance = $model->$field();
+                if ($relationInstance instanceof BelongTo || $relationInstance instanceof HasOne) {
+                    $relatedClass = ($relationInstance instanceof BelongTo) ? $relationInstance->parentClassName : $relationInstance->childClassName;
+                    $value = $relatedClass::mapRawToModel($value);
+                } else if ($relationInstance instanceof HasMany) {
+                    $value = $relationInstance->childClassName::mapRawToModel($value);
+                } else {
+                  throw new Exception(sprintf("Undefined type of relation for %s relation", $field));
+                }
+                unset($relationInstance);
+            }
+            else if (array_key_exists($field, static::$casts)) {
                 $value = static::cast($field, $value);
             }
             $model->$field = $value;
